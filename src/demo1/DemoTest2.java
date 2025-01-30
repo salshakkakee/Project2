a890f716829c34a2e4058a9160b9c27aae4fe9e0
salsh <salsh@Sally.lan>
2025-01-29 21:11:02 -0500

    adding country and zipcode

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -3,6 +3,11 @@ package demo1;
 public class DemoTest2 {
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		String country = "USA";
+		int zipcode = 12345;
+		
+		System.out.println(country + zipcode);
 	}
 
 }
